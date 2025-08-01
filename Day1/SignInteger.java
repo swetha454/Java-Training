84878b84370289d2c104973a3c1270fc6dbe3de9
swetha454 <swethav454@gmail.com>
2025-08-01 21:31:08 +0530

    Create SignInteger.java

---
+++ Day1/SignInteger.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class SignInteger {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>=0){
+            System.out.println("+"+n);
+        }
+        else{
+            System.out.println(n);
+        }
+    }
+}

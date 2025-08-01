2ea9b579bfd1bbf106c0ac222ccf50ec29edbb5b
swetha454 <swethav454@gmail.com>
2025-08-01 21:34:38 +0530

    Create Digits.java

---
+++ Day1/Digits.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Digits {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        System.out.printf("%05d\n",n1);
+        System.out.printf("%5d",n2);
+    }
+}

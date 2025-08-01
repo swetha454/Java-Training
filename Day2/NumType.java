7b5d8b46ddf8c797bc7f6e487e7a781aad01b370
swetha454 <swethav454@gmail.com>
2025-08-01 21:42:39 +0530

    Create NumType.java

---
+++ Day2/NumType.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class NumType {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%2!=0){
+            System.out.println("ODD");
+        }
+        else{
+            System.out.println("EVEN");
+        }
+    }
+}

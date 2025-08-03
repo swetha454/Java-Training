8c54776bbc7fea9099b294877ccce3cdd49842db
swetha454 <swethav454@gmail.com>
2025-08-03 12:06:09 +0530

    Create EvenNumbes.java

---
+++ Day4-Looping/EvenNumbes.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        for(int i=n1;i<=n2;i++){
+            if(i%2==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}

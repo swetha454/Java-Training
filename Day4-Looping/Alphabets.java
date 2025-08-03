0284800de413c02f995ac5797a3a5350a5ee4e31
swetha454 <swethav454@gmail.com>
2025-08-03 12:13:03 +0530

    Create Alphabets.java

---
+++ Day4-Looping/Alphabets.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.println("a b c d e f g h i j k l m n o p q r s t u v w x y z");
+        }
+    }
+}

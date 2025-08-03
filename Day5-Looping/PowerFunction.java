c3d0196092e547dd18abee47866c91ed7cf66767
swetha454 <swethav454@gmail.com>
2025-08-03 13:28:00 +0530

    Create PowerFunction.java

---
+++ Day5-Looping/PowerFunction.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int n1= sc.nextInt();
+        int n2= sc.nextInt();
+        int result=1;
+        for(int i=1;i<=n2;i++){
+            result=result*n1;
+        }
+        System.out.println(result);
+    }
+}

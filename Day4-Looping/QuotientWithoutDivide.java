af028635d86802c88fddf87c4205f1e3e17c6230
swetha454 <swethav454@gmail.com>
2025-08-03 12:09:57 +0530

    Create QuotientWithoutDivide.java

---
+++ Day4-Looping/QuotientWithoutDivide.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int count=0;
+        while(n1>=n2){
+            n1=n1-n2;
+            count++;
+        }
+        System.out.println(count);
+    }
+}

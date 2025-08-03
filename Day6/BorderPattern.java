dddaa842a63e9946a0df4ac15b51a3203a8b544e
swetha454 <swethav454@gmail.com>
2025-08-03 13:46:35 +0530

    Create BorderPattern.java

---
+++ Day6/BorderPattern.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int row=1;row<=n;row++){
+            for(int col=1;col<=n;col++){
+                if(row==1 || row==n || col==1 || col==n)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}

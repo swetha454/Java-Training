8de7b1f3865b1568bf6c2091250075a261448ba6
swetha454 <swethav454@gmail.com>
2025-08-03 13:45:21 +0530

    Create RhombusPattern.java

---
+++ Day6/RhombusPattern.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int row=1;row<=n;row++){
+            for(int space=1;space<=n-row;space++){
+                System.out.print(" ");
+            }
+            for(int star=1;star<=n;star++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

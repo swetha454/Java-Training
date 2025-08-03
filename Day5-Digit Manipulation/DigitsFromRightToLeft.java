6da2b7b26d6778ed22bf8b15ca895bed21f70f2c
swetha454 <swethav454@gmail.com>
2025-08-03 13:37:30 +0530

    Create DigitsFromRightToLeft.java

---
+++ Day5-Digit Manipulation/DigitsFromRightToLeft.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        long n=sc.nextLong();
+        while(n>0){
+            System.out.print(n%10 + " ");
+            n=n/10;
+        }
+    }
+}
